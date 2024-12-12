1a0d127d324a58bb06d52a375de7e608cb040cd3
Jonathan Clemons <j-clemons@tuta.io>
2024-12-12 09:39:04 -0600

    add test for util.findFileDir

---
+++ util/get_project_root_test.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/j-clemons/dbt-language-server/testutils"
+)
+
+func TestFindFileDir(t *testing.T) {
+    expectedTestDataPath, err := testutils.GetTestDataPath("")
+    expected := filepath.Join(
+        expectedTestDataPath,
+        "jaffle_shop_duckdb",
+    )
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    testdataPath, err := testutils.GetTestDataPath("jaffle_shop_duckdb/models")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    fileDir, err := findFileDir("dbt_project.yml", testdataPath)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+	if fileDir != expected {
+		t.Errorf("expected %q, got %q", expected, fileDir)
+	}
+}
