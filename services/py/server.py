7a757a0de31e256978c3ac38395ec055ca1c1b1a
Jonathan <j-clemons@tuta.io>
2025-01-01 15:36:38 -0600

    override the provided sqlfluff config

--- services/py/server.py
+++ services/py/server.py
@@ -10,8 +10,10 @@ class MyServiceServicer(service_pb2_grpc.MyServiceServicer):
     def Lint(self, request, context):
         print("Received file")
         config = FluffConfig.from_path(
-            path=request.sqfluff_cfg_path, overrides={"templater": "jinja"}
+            path=request.sqfluff_cfg_path,
+            overrides={"templater": "jinja"}
         )
+        config.set_value(['ignore'], 'templating')
         lint_results = sqlfluff.lint(request.file_string, config=config)
         cleaned_lint_results = []
         for lr in lint_results:
