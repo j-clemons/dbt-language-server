7e6063783f9b17b485f004f811f85955ea860817
Jonathan <j-clemons@tuta.io>
2025-07-09 23:31:21 -0500

    update version to v0.1.0

--- lsp/initialize.go
+++ lsp/initialize.go
@@ -60,7 +60,7 @@ func NewInitializeResponse(id int) InitializeResponse {
 			},
 			ServerInfo: ServerInfo{
 				Name:    "dbt-language-server",
-				Version: "v0.1.0-beta",
+				Version: "v0.1.0",
 			},
 		},
 	}
